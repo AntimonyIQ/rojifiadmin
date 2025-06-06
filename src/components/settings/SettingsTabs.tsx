373094e01a691375f4a0e97e47e0a3cdbc748097
William <anazawilliam1@gmail.com>
2025-06-06 15:37:31 +0100

    manage processor loading state

--- src/components/settings/SettingsTabs.tsx
+++ src/components/settings/SettingsTabs.tsx
@@ -190,6 +190,55 @@ export default function SettingsTabs({
         </TabsContent>
 
         <TabsContent value="manage-processors">
+          {isProcessorsLoading && (
+            <Card>
+              <CardContent className="space-y-8 px-6 py-6">
+                {/* Header Section */}
+                <div className="space-y-4">
+                  <div className="flex items-center justify-between">
+                    <div className="space-y-2">
+                      <Skeleton className="h-6 w-48" />
+                      <Skeleton className="h-4 w-96" />
+                    </div>
+                    <Skeleton className="h-6 w-32 rounded-md" />
+                  </div>
+
+                  {/* Button */}
+                  <div className="flex justify-end">
+                    <Skeleton className="h-10 w-40 rounded-md" />
+                  </div>
+
+                  {/* Channel Card Skeleton */}
+                  <Card className="border overflow-hidden">
+                    <div className="flex items-center justify-between px-6 py-4">
+                      <div className="flex items-center space-x-4">
+                        <Skeleton className="h-10 w-10 rounded-md" />
+                        {/* Icon */}
+                        <div className="space-y-2">
+                          <Skeleton className="h-4 w-40" />
+                          <Skeleton className="h-3 w-60" />
+                        </div>
+                      </div>
+                      <Skeleton className="h-5 w-12 rounded-full" />
+                    </div>
+                  </Card>
+                  <Card className="border overflow-hidden">
+                    <div className="flex items-center justify-between px-6 py-4">
+                      <div className="flex items-center space-x-4">
+                        <Skeleton className="h-10 w-10 rounded-md" />
+                        {/* Icon */}
+                        <div className="space-y-2">
+                          <Skeleton className="h-4 w-40" />
+                          <Skeleton className="h-3 w-60" />
+                        </div>
+                      </div>
+                      <Skeleton className="h-5 w-12 rounded-full" />
+                    </div>
+                  </Card>
+                </div>
+              </CardContent>
+            </Card>
+          )}
           {processors && <PaymentProcessorsForm data={processors} />}
         </TabsContent>
 
