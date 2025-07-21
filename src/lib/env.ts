c5d2b3e15b585f7607aad5ace7540029d160d036
William <anazawilliam1@gmail.com>
2025-07-21 18:57:07 +0100

    prod baseURL changed

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,7 @@
 const env = {
   VITE_API_BASE_URL:
     import.meta.env.MODE === "production"
-      ? "https://rojifi.qurulab.com"
+      ? "https://rojifi.qurulab.com/api/v1"
       : "https://kd56qjzw-8081.uks1.devtunnels.ms/api/v1",
   ENV: import.meta.env.MODE,
 };
