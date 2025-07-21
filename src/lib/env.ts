8cd71f1dc2d99b91739e3d98961780167c401572
William <anazawilliam1@gmail.com>
2025-07-21 17:04:42 +0100

    base url changed

---
+++ src/lib/env.ts
@@ -0,0 +1,9 @@
+const env = {
+  VITE_API_BASE_URL:
+    import.meta.env.MODE === "production"
+      ? "https://rojifi.qurulab.com"
+      : "https://kd56qjzw-8081.uks1.devtunnels.ms/api/v1",
+  ENV: import.meta.env.MODE,
+};
+
+export default env;
