daad8eae895666da9f0e87f1adf72a0c223f1615
AntimonyIQ <antimonyiq@gmail.com>
2025-08-16 17:59:25 -0700

    Update API base URL and clean up environment configuration

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,10 +1,6 @@
 const env = {
-  VITE_API_BASE_URL:
-    import.meta.env.MODE === "production"
-      ? "https://rojifi.qurulab.com/api/v1"
-      : "https://kd56qjzw-8081.uks1.devtunnels.ms/api/v1",
+  VITE_API_BASE_URL: "https://api.rojifi.com/api/v1",
   ENV: import.meta.env.MODE,
-  
 };
 
 export default env;
